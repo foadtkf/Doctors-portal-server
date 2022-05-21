67192086cf22b1f5566e7f96508ce1d672372116
foadtkf <fakhrulif@gmail.com>
2022-05-22 02:21:46 +0600

    server updated for admin role

--- index.js
+++ index.js
@@ -42,9 +42,32 @@ async function run(){
       const services = await cursor.toArray();
       res.send(services);
     });
-    app.get('/user',async(req,res)=>{
+    app.get('/user',verifyJWT,async(req,res)=>{
       const users =await userCollection.find().toArray()
       res.send(users)
+    })
+    app.get('/admin/:email', async(req, res) =>{
+      const email = req.params.email;
+      const user = await userCollection.findOne({email: email});
+      const isAdmin = user.role === 'admin';
+      res.send({admin: isAdmin})
+    })
+    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+      else{
+        res.status(403).send({message: 'forbidden'});
+      }
+
     })
     app.put('/user/:email',async(req,res)=>{
       const email = req.params.email
