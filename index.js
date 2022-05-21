9532651d0224bf53b7b1d7daf2b86e97dc09ad30
foadtkf <fakhrulif@gmail.com>
2022-05-21 23:14:30 +0600

    jwt verify user added

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
+
+
+// JWT STARTED FROM NOW
+
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
-
+const jwt = require('jsonwebtoken');
 app.use(cors())
 app.use(express.json())
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { decode } = require('jsonwebtoken');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jrofh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri)
+function verifyJWT(req,res,next){
+  const authHeader =req.headers.authorization
+  if(!authHeader){
+    return res.status(401).send('unauthorized user')
+  }
+  const token= authHeader.split(' ')[1]
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET , function(err, decoded) {
+    if(err){
+      return res.status(403).send('forbidden access')
+    }
+    req.decoded =decoded;
+    next()
+  });
+}
 
 async function run(){
   try{
     await client.connect();
     const serviceCollection = client.db('doctors_portal').collection('services');
     const bookingCollection = client.db('doctors_portal').collection('bookings');
-
+    const userCollection = client.db('doctors_portal').collection('users');
     app.get('/service', async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
     });
-
+    app.get('/user',async(req,res)=>{
+      const users =await userCollection.find().toArray()
+      res.send(users)
+    })
+    app.put('/user/:email',async(req,res)=>{
+      const email = req.params.email
+      const user = req.body
+      const filter = {email:email}
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+      res.send({result,token})
+    })
     // Warning: This is not the proper way to query multiple collection. 
     // After learning more about mongodb. use aggregate, lookup, pipeline, match, group
     app.get('/available', async(req, res) =>{
@@ -52,7 +85,6 @@ async function run(){
 
       res.send(services);
     })
-
     /**
      * API Naming Convention
      * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
@@ -61,12 +93,17 @@ async function run(){
      * app.patch('/booking/:id) //
      * app.delete('/booking/:id) //
     */
-
-    app.get('/booking', async(req, res) =>{
+    app.get('/booking', verifyJWT,async(req, res) =>{
       const patient = req.query.patient;
+      const decodedEmail = req.decoded.email
+      if(patient==decodedEmail){
       const query = {patient: patient};
       const bookings = await bookingCollection.find(query).toArray();
       res.send(bookings);
+      }
+      else{
+        return res.status(403).send({message: 'forbidden access'})
+      }
     })
 
     app.post('/booking', async (req, res) => {
